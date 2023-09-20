8891a9a379ff5cae909c661b3228c9cf425f1e2a
EricJKL33 <eric.jkl@icloud.com>
2023-09-20 16:30:12 +0200

    sessions

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,27 +1,37 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 
-const tasks = [
-  {
-    title: "Example task 1",
-    done: false,
-  },
-  {
-    title: "Example task 2",
-    done: true,
-  },
-];
+// const tasks = [
+//   {
+//     title: "Example task 1",
+//     done: false,
+//   },
+//   {
+//     title: "Example task 2",
+//     done: true,
+//   },
+// ];
 
-console.log(tasks);
+// console.log(tasks);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.set("view engine", "ejs");
 
 app.post("/task", (req, res) => {
   if (req.body.task) {
-    tasks.push({
+    req.session.tasks.push({
       title: req.body.task,
       done: false,
     });
@@ -30,21 +40,24 @@ app.post("/task", (req, res) => {
 });
 
 app.get("/task/:id/done", (req, res) => {
-  if (tasks[req.params.id]) {
-    tasks[req.params.id].done = true;
+  if (req.session.tasks[req.params.id]) {
+    req.session.tasks[req.params.id].done = true;
   }
   res.redirect("/");
 });
 
 app.get("/task/:id/delete", (req, res) => {
-  if (tasks[req.params.id]) {
-    tasks.splice(req.params.id, 1);
+  if (req.session.tasks[req.params.id]) {
+    req.session.tasks.splice(req.params.id, 1);
   }
   res.redirect("/");
 });
 
 app.get("/", (req, res) => {
-  res.render("todolist", { tasks: tasks });
+  if (!req.session.tasks) {
+    req.session.tasks = [];
+  }
+  res.render("todolist", { tasks : req.session.tasks });
 });
 
 app.listen(port, () => {
