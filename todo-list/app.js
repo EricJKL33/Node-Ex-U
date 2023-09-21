0a2b36699eb0121837298f85448bcc2f20bd4e25
Eric Gicquel <eric.gicquel@icloud.com>
2023-09-21 16:21:29 +0200

    lalala

--- todo-list/app.js
+++ todo-list/app.js
@@ -25,11 +25,6 @@ app.get("/", (req, res) => {
   res.render("todolist", { tasks: tasks });
 });
 
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
-
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
