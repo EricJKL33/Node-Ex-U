4f3cc83ccae4bfedf86ac95e09f7e91f1f759321
Eric Gicquel <eric.gicquel@icloud.com>
2023-09-27 17:45:49 +0200

    tuto mongodb

---
+++ mongoose/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/exemple-mongoose");
+
+  console.log("Connexion ok");
+}
+
+main();
