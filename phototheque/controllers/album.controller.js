ad1074f4f145c75ae3954821ee08873776e47130
Eric Gicquel <eric.gicquel@icloud.com>
2023-10-10 17:33:14 +0200

    uploadImg

--- phototheque/controllers/album.controller.js
+++ phototheque/controllers/album.controller.js
@@ -1,13 +1,51 @@
 const Album = require("../models/Album");
+const path = require("path");
 
 const albums = async (req, res) => {
   const albums = await Album.find();
-  console.log(albums)
+  console.log(albums);
 
   res.render("albums", {
     title: "Mes albums",
     albums,
   });
+};
+
+const album = async (req, res) => {
+  try {
+    const idAlbum = req.params.id;
+    const album = await Album.findById(idAlbum);
+
+    // console.log(album);
+
+    res.render("album", {
+      title: "Album",
+      album,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/404");
+  }
+};
+
+const addImage = async (req, res) => {
+  const idAlbum = req.params.id;
+  const album = await Album.findById(idAlbum);
+
+  console.log(req.files);
+
+  const imageName = req.files.image.name;
+
+  const folderPath = path.join(__dirname, "../public/uploads", idAlbum);
+  fstat.mkdirSync(folderPath, { recursive: true });
+
+  const localPath = path.join(folderPath, imageName);
+  await req.files.image.mv(localPath);
+
+  album.images.push(imageName);
+  await album.save();
+
+  res.redirect(`/albums/${idAlbum}`);
 };
 
 const createAlbumForm = (req, res) => {
@@ -37,4 +75,10 @@ const createAlbum = async (req, res) => {
   }
 };
 
-module.exports = { albums, createAlbumForm, createAlbum };
+module.exports = {
+  albums,
+  album,
+  addImage,
+  createAlbumForm,
+  createAlbum,
+};
